f4a8a35045b7f5d22dec2b6f29dc8cdf4afa7faf
tal42367 <tal42eisakov@gmail.com>
2026-09-22 07:17:21 +0300

    Yishai v0.6 version

--- yishai-parasha/app/build.gradle.kts
+++ yishai-parasha/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.yishai.parasha"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.5.0"
+        versionCode = 7
+        versionName = "0.6.0"
     }
 }
 
