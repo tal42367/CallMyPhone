4200d5ec42d4c776a865e4047d2b6816a0c49ca7
tal42367 <tal42eisakov@gmail.com>
2026-09-22 07:40:57 +0300

    Yishai v0.7.1 male interviewer voice

--- yishai-parasha/app/build.gradle.kts
+++ yishai-parasha/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.yishai.parasha"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.7.0"
+        versionCode = 9
+        versionName = "0.7.1"
     }
 }
 
