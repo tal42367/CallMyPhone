e58018506a917b659c24e62054ed13e2924dcc89
tal42367 <tal42eisakov@gmail.com>
2026-09-25 06:33:18 +0300

    Bump Yishai app to v0.9

--- yishai-parasha/app/build.gradle.kts
+++ yishai-parasha/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yishai.parasha"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.8.0"
+        versionCode = 11
+        versionName = "0.9.0"
     }
 
     compileOptions {
